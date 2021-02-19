0c096f06fe00c483c637b62c72b1c0f26b93a779
Kamil Prusko <kamilprusko@gmail.com>
2021-02-19 16:51:01 +0100

    WIP

--- theme/js/main.js
+++ theme/js/main.js
@@ -1,37 +1,56 @@
-
 document.addEventListener('DOMContentLoaded', () => {
+    // Menu
     const menuBurger = document.getElementById('header-burger');
 
+    function openMenu() {
+        let target = document.getElementById(menuBurger.dataset['target']);
+
+        menuBurger.classList.add('is-active');
+        target.classList.add('is-active');
+    }
+
+    function closeMenu() {
+        let target = document.getElementById(menuBurger.dataset['target']);
+
+        menuBurger.classList.remove('is-active');
+        target.classList.remove('is-active');
+    }
+
+    function isMenuOpened() {
+        return menuBurger.classList.contains('is-active');
+    }
+
     menuBurger.addEventListener('click', () => {
-        const target = document.getElementById(menuBurger.dataset.target);
+        if (!isMenuOpened()) {
+            openMenu();
+            document.documentElement.classList.add('is-clipped');
+            document.documentElement.scroll(0, 0)
+        }
+        else {
+            closeMenu();
+            document.documentElement.classList.remove('is-clipped');
+        }
+    });
 
-        menuBurger.classList.toggle('is-active');
-        target.classList.toggle('is-active');
-        document.documentElement.classList.toggle('is-clipped');
+    // Smooth scrolling
+    function scrollToAnchor() {
+        debugger;
+    }
 
-        // scroll to top
-        document.documentElement.scroll(0, 0)
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', event => {
+            let href = anchor.getAttribute('href');
+            try {
+                document.querySelector(href).scrollIntoView({
+                    behavior: 'smooth'
+                });
+                history.replaceState(null, '', href)
+                event.preventDefault();
+            } catch (error) {
+                console.warn(error);
+
+                // falls back to default behavior
+            }
+        });
     });
 });
-
-
-/*
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-        // Add a click event on each of them
-        $navbarBurgers.forEach( el => {
-            el.addEventListener('click', () => {
-                // Get the target from the "data-target" attribute
-                const target = el.dataset.target;
-                const $target = document.getElementById(target);
-
-                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-                el.classList.toggle('is-active');
-                $target.classList.toggle('is-active');
-            });
-        });
-    }
-*/
